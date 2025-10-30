d9d8d527ccc5aa75651800d43a89f7431a6567e8
lucarin91 <lucarin@protonmail.com>
2025-10-30 16:55:12 +0100

    remove not used libraries

--- internal/orchestrator/app/generator/app_template/sketch/sketch.ino
+++ internal/orchestrator/app/generator/app_template/sketch/sketch.ino
@@ -1,15 +1,9 @@
-#include <Arduino_RouterBridge.h>
-
 void setup() {
-  // setup the bridge for comunicating with the Linux subsystem.
-  Bridge.begin();
-
-  // start the monitor for debugging logs.
-  Monitor.begin();
-
   // put your setup code here, to run once:
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
 }
